9d8d1109f701287fb0c9d8f4bfa87b6b7cbaeb10
Ying <jhihying.wu@tkb.com.tw>
2026-08-17 10:25:17 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import HelloWorld from "./components/HelloWorld.vue";
+</script>
+
+<template>
+  <!-- <HelloWorld /> -->
+  <H1 class="text-red-300 text-2xl">Test</H1>
+</template>
